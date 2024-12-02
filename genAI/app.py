f4db752aa2a98ab28e3e93b3acfe066d87eb1c82
nihal0610 <168910699+nihal0610@users.noreply.github.com>
2024-12-02 11:12:13 +0530

    Update app.py

--- genAI/app.py
+++ genAI/app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from openai import AzureOpenAI
 from langchain.agents.agent_types import AgentType
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
-#from langchain.chat_models import ChatOpenAI
+from langchain.chat_models import ChatOpenAI
 from dotenv import load_dotenv
 from datetime import datetime
 from langchain_community.chat_models import ChatOpenAI
