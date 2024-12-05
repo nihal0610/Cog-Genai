791b5427462f18d8d0e250df25aeada3f7f297f5
nihal0610 <168910699+nihal0610@users.noreply.github.com>
2024-12-05 16:02:54 +0530

    Update app.py

--- genAI/app.py
+++ genAI/app.py
@@ -62,7 +62,7 @@ if uploaded_file is not None and openai_api_key:
              filtered_df = ddf[ddf["Manager ID"].isin(value)][["Manager ID", "Project Name", "Project Id"]]
              st.write(filtered_df)
              filtered_df = pd.DataFrame()
-             st.write(filtered_df)
+             value= []
              
              # Save to Excel
              # filtered_df.to_excel('output.xlsx', index=False)
